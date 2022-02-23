0b625377eaa933ddb0653cf2dfc9fc55af754a2c
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-23 21:20:24 +0300

    add browser size for tests

--- src/test/java/samoiloff90/github/io/tests/TestBase.java
+++ src/test/java/samoiloff90/github/io/tests/TestBase.java
@@ -1,13 +1,17 @@
 package samoiloff90.github.io.tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
     @BeforeEach
     void precondition() {
         open("https://www.dns-shop.ru/");
+        browserSize = "1920x1080";
     }
 
     @AfterEach
