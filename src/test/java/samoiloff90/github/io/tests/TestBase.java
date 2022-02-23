1ffc0f713ad5795eef4d035c40eadff8a553282f
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-23 17:18:06 +0300

    add precondition and close browser

--- src/test/java/samoiloff90/github/io/tests/TestBase.java
+++ src/test/java/samoiloff90/github/io/tests/TestBase.java
@@ -1,5 +1,17 @@
 package samoiloff90.github.io.tests;
 
-public class TestBase {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static com.codeborne.selenide.Selenide.*;
 
+public class TestBase {
+    @BeforeEach
+    void precondition() {
+        open("https://www.dns-shop.ru/");
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        closeWebDriver();
+    }
 }
