ac2a4eadd8c7eabb0c4789d49a614517e90ae142
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-25 23:42:25 +0300

    removed unused imports

--- src/test/java/samoiloff90/github/io/tests/TestBase.java
+++ src/test/java/samoiloff90/github/io/tests/TestBase.java
@@ -1,12 +1,8 @@
 package samoiloff90.github.io.tests;
-
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.*;
 
