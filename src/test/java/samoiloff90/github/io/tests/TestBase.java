84fac88647bd4c7007f7359ed60eb66efecf74ba
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-02-25 23:34:21 +0300

    add listenner tests for allure reports

--- src/test/java/samoiloff90/github/io/tests/TestBase.java
+++ src/test/java/samoiloff90/github/io/tests/TestBase.java
@@ -1,6 +1,9 @@
 package samoiloff90.github.io.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -10,6 +13,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestBase {
     @BeforeEach
     void precondition() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://www.dns-shop.ru/");
         browserSize = "1920x1080";
     }
